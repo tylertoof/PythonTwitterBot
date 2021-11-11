e7875e8792b170d33e295eedc0b56ff94d4738a1
tylertoof <94020412+tylertoof@users.noreply.github.com>
2021-11-11 12:59:58 -0600

    Delete keys.py

--- keys.py
+++
@@ -1,7 +0,0 @@
-
-keys = dict(
-    consumer_key='5Fhs0RyjYJIjt9EaIXSYuSD6d',
-    consumer_secret='iVlJj00xrqIuDT0V4LA2zU8SjyqUbHIJ7D68kQvsZlxyPe1VV3',
-    access_token='1073193791513944065-o1Ze6pbK7A3fARhLLhpWs50F4rvPlH',
-    access_token_secret='SmJfMabFGF71CAjGI3tkEuJcrILIFZyf5MttIX9Ke5705',
-)
